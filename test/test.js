6da012f4dbcc8cacbef6b020a4679af80e1a55bb
Apetu <apetugideon@gmail.com>
2019-11-12 00:00:20 +0100

    feature(gifs):wrap up the gif module [Fixing the the automated testing tingy]

--- test/test.js
+++ test/test.js
@@ -51,7 +51,7 @@ describe ("User Management Module Testing", function(){
                   .delete('/api/v1/auth/'+res.body.data.userId)
                   .set({"Authorization" : "Bearer " + token})
                   .end((err, res) => {
-                    //console.log(err, res);
+                    console.log(err, res);
                     res.should.have.status(201);
                     res.body.should.be.a('object');
                     res.body.should.have.property('status').eql('success');
