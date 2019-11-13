6385ae8a580827cc06278c606e8998ff5cf4b560
Apetu <apetugideon@gmail.com>
2019-11-13 19:50:12 +0100

    Fixing the unit testing thingy

--- test/gifs.js
+++ test/gifs.js
@@ -2,7 +2,7 @@ let dbconn = require("../dbconn");
 let chai = require("chai");
 let chaiHttp = require("chai-http");
 let server = require("../server"); 
-//let should = chai.should();
+let should = chai.should();
 chai.use(chaiHttp);
 
 server = "https://teamwork-heroku-staging.herokuapp.com";
