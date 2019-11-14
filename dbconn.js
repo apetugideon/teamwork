ecd724639c3ee50dce990648cce19b1376687fab
Apetu <apetugideon@gmail.com>
2019-11-14 18:40:45 +0100

    Modification to the db connection

--- dbconn.js
+++ dbconn.js
@@ -5,8 +5,10 @@ const env = process.env.NODE_ENV || 'development';
 let connString = {};
 
 if (!process.env.DATABASE_URL) {
+  console.log("Here 1");
   connString = require("./dbparam");
 } else {
+  console.log("Here 2");
   connString = {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
