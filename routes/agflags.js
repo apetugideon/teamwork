8b89c24d4a11d6088e96c6fa63e6e91750dce6a3
Apetu <apetugideon@gmail.com>
2019-11-16 22:35:42 +0100

    Bug fixing

--- routes/agflags.js
+++ routes/agflags.js
@@ -4,8 +4,12 @@ const auth      = require('../middleware/auth');
 
 const agflagsController = require('../controllers/agflagsController');
 
+router.get('/', auth, agflagsController.getAllAgflags);
 router.post('/', auth, agflagsController.createAgflag);
+router.get('/:id', auth, agflagsController.getOneAgflag);
+router.put('/:id', auth, agflagsController.modifyAgflag);
 router.delete('/:id', auth, agflagsController.deleteAgflag);
+
 
 module.exports = router;
 
